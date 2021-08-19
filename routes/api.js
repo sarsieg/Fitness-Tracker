2832c044eec537728dc6538df24c9997434590fe
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-19 16:55:23 -0500

    adding router.get function in api.js file to find all workouts and to get the total duration of the workouts

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const db = require("../models");
+const router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+    })
+})
