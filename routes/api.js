86e03264f6409af86597eefd76e30c6da3605736
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-19 17:01:32 -0500

    added router.post function to create a new workout and catch and error if one occurs

--- routes/api.js
+++ routes/api.js
@@ -28,3 +28,12 @@ router.put("/api/workouts/:id", (req, res) => {
     });
 
 });
+
+// creates workout 
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
