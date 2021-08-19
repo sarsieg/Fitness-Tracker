649b3ca1ea90f63475a4e1f4f1fbc5d1d842294c
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-19 15:13:43 -0500

    finished adding code to server.js file as well as added routes to the js files and listening port

--- server.js
+++ server.js
@@ -19,3 +19,10 @@ mongoose.connect(
         useFindAndModify: false
     }
 );
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
