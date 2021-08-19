ac855d09c40af5b604510993f0d66d21bfe4443d
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-19 16:59:16 -0500

    added router.put function to find one workout and to update it. as well as added a .catch json to throw an error if one occurs

--- routes/api.js
+++ routes/api.js
@@ -10,5 +10,21 @@ router.get("/api/workouts", (req, res) => {
             });
             workout.totalDuration = total;
         });
-    })
-})
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+// Add the exercise
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body }
+    }, { new: true }).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
