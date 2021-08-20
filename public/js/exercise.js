e06c0dd9fa1088c41bbc14afe9c68b78083d924f
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-19 20:38:17 -0500

    added prevent default and workout data information

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -89,3 +89,23 @@ function validateInputs() {
         addButton.setAttribute("disabled", true);
     }
 }
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    let workoutData = {};
+
+    if (workoutType === "cardio") {
+        workoutData.type = "cardio";
+        workoutData.name = cardioNameInput.value.trim();
+        workoutData.distance = Number(distanceInput.value.trim());
+        workoutData.duration = Number(durationInput.value.trim());
+    } else if (workoutType === "resistance") {
+        workoutData.type = "resistance";
+        workoutData.name = nameInput.value.trim();
+        workoutData.weight = Number(weightInput.value.trim());
+        workoutData.sets = Number(setsInput.value.trim());
+        workoutData.reps = Number(repsInput.value.trim());
+        workoutData.duration = Number(resistanceDurationInput.value.trim());
+    }
+}
