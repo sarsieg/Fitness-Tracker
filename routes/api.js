f6ccb1f653fe5285f32eea0332b70c773deb038a
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-15 00:07:02 -0500

    fixing api.js file

--- routes/api.js
+++ routes/api.js
@@ -1,50 +1,54 @@
 const Workout = require("../models/workout.js");
-const router = express.Router();
-const mongoose = require("mongoose");
-const express = require("express");
 
-// creates workout 
-router.post("/api/workouts", ({ body }, res) => {
-            Workout.create({})
-                .then((dbWorkout => {
-                        res.json(dbWorkout);
-                    })
-                    .catch(({ message }) => {
-                        console.log(message);
-                    });
-                });
+module.exports = function(app) {
+    app.get("/api/workouts", function(req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
 
-        router.put("/api/workouts/:id", ({ params, body }, res) => {
-            console.log("PARAMS", body, params);
 
-            Workout.findByIdAndUpdate({ _id: params.id }, { $push: { exercises: body } }, { new: true })
-                .then((dbWorkout) => {
-                    res.json(dbWorkout);
-                })
-                .catch((err) => {
-                    res.json(err);
-                });
-        });
+    // creates workout 
+    app.post("/api/workouts", function(req, res) {
+        Workout.create({})
+            .then(data =>
+                res.json(data))
+            .catch(err => {
+                res.json(err)
+            })
+    });
 
-        // gets the workouts within range
-        router.get("/api/workouts/range", (req, res) => {
-            Workout.find({})
-                .limit(7)
-                .then((dbWorkout) => {
-                    res.json(dbWorkout);
-                }).catch(err => {
-                    res.json(err);
-                });
-        });
+    // gets the workouts within range
+    router.get("/api/workouts/range", function(req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    });
 
-        router.get("/api/workouts", (req, res) => {
-            Workout.find({})
-                .then((dbWorkout) => {
-                    res.json(dbWorkout);
-                })
-                .catch((err) => {
-                    res.json(err);
-                });
-        });
+    app.post("/api/workouts/range", function(req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
 
-        module.exports = router;
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    router.put("/api/workouts/:id", ({ params, body }, res) => {
+        Workout.findByIdAndUpdate({ _id: params.id }, { $push: { exercises: body } }, { new: true, runValidators: true })
+            .then(data => res.json(data))
+            .catch((err) => {
+                res.json(err);
+            })
+    });
+}
